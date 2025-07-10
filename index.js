663a2c90fa12a8a61ad11adff875ef1390b12eeb
Pavel S <pavlikochka@gmail.com>
2025-07-10 14:09:27 +0300

    add 18 years old check in develop

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+// main - на 3 строке написано:
+
 console.log('Hellow world')
 
+
+// develop - на этой ветке в 3 строке написано:
+
+console.log('Hello JavaScript')
+
+
+// develop -> main
+
+
+const age = 20
+if (age >= 18) {
+    console.log('Вы совершеннолетний')
+} else {
+    console.log('Вы несовершеннолетний')
+}
